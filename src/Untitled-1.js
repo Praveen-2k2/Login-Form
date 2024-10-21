b20237b15a5cb6078f1681c1ab028907834e704b
Praveen-2k2 <posinapraveen2002@gmail.com>
2024-10-21 13:21:34 +0530

    Update Untitled-1.js

--- src/Untitled-1.js
+++ src/Untitled-1.js
@@ -1,74 +1,74 @@
-import React from 'react'
+// import React from 'react'
 
-const Untitled-1 = () => {
-const [user, setUser] = useState({
-  firstName: '',
-  lastName: '',
-  email: '',
-  mobileNumber: '',
-  gender: '',
-  dateOfBirth: ''
-});
-const { firstName, lastName, email, mobileNumber, gender, dateOfBirth } = user;
+// const Untitled-1 = () => {
+// const [user, setUser] = useState({
+//   firstName: '',
+//   lastName: '',
+//   email: '',
+//   mobileNumber: '',
+//   gender: '',
+//   dateOfBirth: ''
+// });
+// const { firstName, lastName, email, mobileNumber, gender, dateOfBirth } = user;
 
-const [error, setError] = useState({});
-const changeHandler = (e) => {
-  user[e.target.name] = e.target.value;
-  setUser({ ...user }, handleValidate(e));
-}
+// const [error, setError] = useState({});
+// const changeHandler = (e) => {
+//   user[e.target.name] = e.target.value;
+//   setUser({ ...user }, handleValidate(e));
+// }
 
-const handleValidate = (e) => {
-    let input = e.target;
-    switch (input.name || input.tagName) {
-      case "firstName":
-        error.firstName = validate_city(input.value);
-        break;
-      default:
-        break;
-    }
-    setError({ ...error });
-  }
+// const handleValidate = (e) => {
+//     let input = e.target;
+//     switch (input.name || input.tagName) {
+//       case "firstName":
+//         error.firstName = validate_city(input.value);
+//         break;
+//       default:
+//         break;
+//     }
+//     setError({ ...error });
+//   }
  
-  const validateAll = () => {
-    let { firstName, lastName, email, datOfBirth } = userForm;
-    let errors = {};
-    errors.job_title_id = validate_city(firstName);
-    setError(errors);
-    return errors;
-  }
+//   const validateAll = () => {
+//     let { firstName, lastName, email, datOfBirth } = userForm;
+//     let errors = {};
+//     errors.job_title_id = validate_city(firstName);
+//     setError(errors);
+//     return errors;
+//   }
  
-  const handleSubmit = () => {
-    let errors = validateAll();
-    if (isValid(errors)) {
-      console.log(userForm);
-    }else {
-      let s1 = { error }
-      s1 = errors
-      setError(s1);
-    }
-  }
+//   const handleSubmit = () => {
+//     let errors = validateAll();
+//     if (isValid(errors)) {
+//       console.log(userForm);
+//     }else {
+//       let s1 = { error }
+//       s1 = errors
+//       setError(s1);
+//     }
+//   }
  
-  var alphaNumericWithSpace = new RegExp(/^[a-zA-Z ]+$/);
+//   var alphaNumericWithSpace = new RegExp(/^[a-zA-Z ]+$/);
 
-  export const isValid = (errors) => {
-    let keys = Object.keys(errors);
-    let count = keys.reduce((acc, curr) => (errors[curr] ? acc + 1 : acc), 0);
-    return count === 0;
-  };
+//   export const isValid = (errors) => {
+//     let keys = Object.keys(errors);
+//     let count = keys.reduce((acc, curr) => (errors[curr] ? acc + 1 : acc), 0);
+//     return count === 0;
+//   };
  
  
-  export const validate_city = (value) =>
-    value === "" || value == null
-      ? "This field is required"
-      : !alphaNumericWithSpace.test(value)
-        ? "Please enter valid city" : '';
+//   export const validate_city = (value) =>
+//     value === "" || value == null
+//       ? "This field is required"
+//       : !alphaNumericWithSpace.test(value)
+//         ? "Please enter valid city" : '';
 
 
-        return (
-          <div>
+//         return (
+//           <div>
             
-          </div>
-        )
-      }
+//           </div>
+//         )
+//       }
       
-      export default Untitled-1        
+//       export default Untitled-1        
